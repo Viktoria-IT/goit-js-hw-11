47e9ada12e985576f10f219066c598052475b663
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-07-15 09:00:49 +0300

    add 11

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,36 +1,30 @@
 import '../css/common.css';
 
-const refs = {
-  startBtn: document.querySelector('button[data-start="start"]'),
-  stopBtn: document.querySelector('button[data-stop="stop"]'),
-  body: document.querySelector("body"),
-};
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
 
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
+let isActive = false;
+startRender.addEventListener('click', function () {
+   if (!isActive) {
+      isActive = setInterval(() => {
+         document.body.style.backgroundColor = getRandomHexColor();
+      }, 1000);
+   }
+   else {
+      return;
+   }
 
-// const timer = {
-//   isActive: false,
-//   start() {
-//     if (this.isActive) {
-//       return;
-//     }
+});
 
-//     this.isActive = true;
-//     this.switchColors = setInterval(() => {
-//       const min = 0;
-//       const max = colors.length - 1;
-//       let i = getRandomHexColor(min, max);
-//       refs.body.style.backgroundColor = colors[i];
-//     }, 1000);
-//   },
-//   stop() {
-//     clearInterval(this.switchColors);
-//     this.isActive = false;
-//   },
-// };
 
-refs.startBtn.addEventListener("click", timer.start.bind(timer));
-refs.stopBtn.addEventListener("click", timer.stop.bind(timer));
+
+stopRender.addEventListener('click', function () {
+  if (isActive) {
+    clearInterval(isActive);
+  }
+  isActive = false;
+});
+
+
+
