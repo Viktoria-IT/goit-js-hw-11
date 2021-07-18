98554fead069243b434acafd59225277d33335db
Viktoria Koval <kovalviktoria.hitech@gmail.com>
2021-07-18 16:25:37 +0300

    исправила ошибку в промисах

--- src/js/03-promisify.js
+++ src/js/03-promisify.js
@@ -50,36 +50,28 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const makeTransaction = (transaction) => {
-   return new Promise((res, rej) => {
+  return new Promise((resolve, reject) => {
     const delay = randomIntegerFromInterval(200, 500);
 
     setTimeout(() => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        res(transaction.id, delay);
+        resolve({ id: transaction.id, time: delay });
       } else {
-        rej(transaction.id);
+        reject(transaction.id);
       }
     }, delay);
-  });
+  })
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({ id, time }) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
-const logError = (id) => {
+const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
-
-makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-
-makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
 
 // Currently the function works like this
 // makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
@@ -88,7 +80,7 @@ makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
 // makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
 
 // The function should work like this
-// makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
-// makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 70, amount: 150 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 71, amount: 230 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 72, amount: 75 }).then(logSuccess).catch(logError);
+makeTransaction({ id: 73, amount: 100 }).then(logSuccess).catch(logError);
